bb8f5cbf729cd992fd0ce9165374373d7c4cecf8
Marco <contact.marcusrb@gmail.com>
2018-10-09 08:52:33 +0200

    Update FB_ads_viewContent_example.js

---
+++ FB_ads_viewContent_example.js
@@ -0,0 +1,13 @@
+<!--ProductDetails as ViewContent - Update 10.2018 MR//PD -->
+
+<script>
+  fbq('track', 'ViewContent', {
+    value: {{cJS - EEC - product.detail.price}},  //product detail Price as FLOAT
+    currency: 'EUR',							// currency as STRING
+    content_ids: {{cJS - dlv - product.id}}, //shows first 10 elements only for large ecommerce. objectType ARRAY
+    content_type: 'product',					// choose between products or product_group 
+    product_catalog_id: '{{FB ads - product_catalog_id}}', // ID of product Catalog on Facebook repository
+    content_name: '{{cJS - dlv - productName}}', // name of product, STRING
+    content_category: '{{cJS - dlv - categoryList}}', // category of product, STRING and concat with '>'
+  });
+</script>
