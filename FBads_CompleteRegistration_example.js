226c97cb28a85425a3ca2420c8bcf2b8f812bd92
Marco <contact.marcusrb@gmail.com>
2018-10-09 10:03:27 +0200

    add CompleteRegistration

---
+++ FBads_CompleteRegistration_example.js
@@ -0,0 +1,9 @@
+/* CompleteRegistration as ViewContent for Google Tag Manager - Update 10.2018 MR//PD 
+Trigger: tag works when page view 'completeRegistration' loaded or custom event fired
+Remember: Set Tag Sequencing load before main pageView FBadsTag
+*/
+
+
+<script>
+  fbq('track', 'CompleteRegistration');
+</script>
