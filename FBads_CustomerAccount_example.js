c8f865c6b6f12bd0dd3171d0dbe67d4cc125df60
Marco <contact.marcusrb@gmail.com>
2018-10-09 09:10:05 +0200

    customerAccount

---
+++ FBads_CustomerAccount_example.js
@@ -0,0 +1,10 @@
+/* CustomerAccount as ViewContent for Google Tag Manager - Update 10.2018 MR//PD 
+Trigger will be: when customer start Login during navigation or checkout
+Remember: Set Tag Sequencing load before main pageView FBads tag
+*/
+
+
+<script>
+  fbq('track', 'CustomerAccount');
+</script>
+
