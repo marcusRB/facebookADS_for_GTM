a2fa4bf042e5327eea165e71cdfe6f757939c03c
Marco <contact.marcusrb@gmail.com>
2018-10-09 09:09:58 +0200

    initiateCheckout

---
+++ FBads_InitiateCheckout_example.js
@@ -0,0 +1,11 @@
+/* InitiateCheckout as ViewContent for Google Tag Manager - Update 10.2018 MR//PD 
+Trigger will be: when Step 1 loaded or Step 2 initiate, extract checkout value from dataLayer
+Remember: Set Tag Sequencing load before main pageView FBads tag
+*/
+
+<script>
+  fbq('track', 'InitiateCheckout', {
+    value: {{cJS - EEC - checkoutValue}}, //as FLOAT
+    currency: 'EUR',
+  });
+</script>
