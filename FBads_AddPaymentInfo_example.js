a69f0991af779b8b7cc6fcc63b3a6f7f7423cd01
Marco <contact.marcusrb@gmail.com>
2018-10-09 10:04:02 +0200

    update AddPaymentInfo

--- FBads_AddPaymentInfo_example.js
+++ FBads_AddPaymentInfo_example.js
@@ -1,5 +1,5 @@
 /* AddPaymentInfo event as ViewContent for Google Tag Manager - Update 10.2018 MR//PD 
-Trigger will be: event on selected PaymentInfo
+Trigger: event on selected PaymentInfo
 Remember: Set Tag Sequencing load before main pageView FBads tag
 */
 
